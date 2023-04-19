54bffc3cdc475e1f233cc58c1f8a70a712077578
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-04-19 09:01:19 +0200

    Add background video

--- src/components/PageSectionHero.tsx
+++ src/components/PageSectionHero.tsx
@@ -20,6 +20,10 @@ const PageSectionHero: FC<PageSectionHeroProps> = ({ className, content }) => {
   return (
     <ThemeProvider theme={darkTheme}>
       <HeroSection className={className}>
+        <BackgroundVideo autoPlay muted loop>
+          <source src="/videos/fetamin-background.webm" type="video/webm" />
+          <source src="/videos/fetamin-background.mp4" type="video/mp4" />
+        </BackgroundVideo>
         <PageContainer>
           <ContentWrapper initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
             <Contents>
@@ -64,6 +68,11 @@ const PageContainer = styled.div`
   flex-direction: column;
   flex: 1;
   height: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 `
 
 const ContentWrapper = styled(motion.div)`
@@ -73,3 +82,10 @@ const ContentWrapper = styled(motion.div)`
 `
 
 const Image = styled.img``
+
+const BackgroundVideo = styled.video`
+  filter: grayscale() blur(3px);
+  max-height: 100vh;
+  width: 100%;
+  object-fit: cover;
+`
